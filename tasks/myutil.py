ec4cf0522acdb6b0f6b1b0503bec0ea40d8b2d6f
Tiansi Dong <tdong@uni-bonn.de>
2016-07-13 16:23:37 +0200

    transer rdf (spo) to table (s c1 c2 .. o)

--- tasks/myutil.py
+++ tasks/myutil.py
@@ -7,12 +7,62 @@ def construct_data_frame(g,dim=[]):
     """
     if dim==[]:
         dim = get_all_dimensions(g)
-    qstr_part1 = 'select ?s ' 
+    qstr = qstr_for_rdf_to_one_table(g, dim=dim)
+    cols = ['ObservationID'] + dim + ['Measure']
+    qrlt = get_query_result_toPython(g, qstr) 
+    """
+    qrlt is a list of column values -- 'observation-id, dim[0], dim[1], ...,dim[n], 'measure'
+    one row of get_query_result(g, qstr) looks as follows
+    for example: (rdflib.term.URIRef('http://data.openbudgets.eu/resource/dataset/aragon-2006-income/observation/42'), 
+                  rdflib.term.URIRef('http://data.openbudgets.eu/resource/codelist/estructura_financiacion_i_aragon_2006/91003'), 
+                  rdflib.term.URIRef('http://data.openbudgets.eu/resource/codelist/estructura_economica_i_aragon_2006/320004'), 
+                  rdflib.term.Literal('20000.0', datatype=rdflib.term.URIRef('http://www.w3.org/2001/XMLSchema#decimal'))) 
+    one row of get_query_result_toPython(g, qstr) looks as follows
+                [42, 91003, 370004, 20000.0]
+    """
+    
+    
+    
+    
     
     return dimLst
     
-def str_of_dim_vars(stem):
-    pass
+def qstr_for_rdf_to_one_table(g, dim=[]):
+    qstr='select ?s '+ create_str_of_vars('?d', len(dim)) +' ?o   \
+             where { '+ create_str_of_spo_vars('?s', varStems=['?p','?d'], num=len(dim)) +'\
+                         ?s ?m ?o .  \
+                         filter(contains(str(?s), "observation")   '+  create_filter_conditions('?p', dim) +'\
+                               && contains(str(?m),  "measure")   \
+                                   )  }'
+    return qstr
+
+def create_str_of_spo_vars(headStem, varStems=['?p','?d'], num=1):
+    """
+    function example: create_str_of_spo_vars(?s, varStems=['?p',?d], 3) ==> '?s ?p1 ?d1 . ?s ?p2 ?d2 . ?s ?p3 ?d3 . '
+    """
+    rlt = ""
+    for i in range(1, num+1):
+        rlt +=headStem +' ' + ' '.join(map(lambda s:s+str(i), varStems))+ " . "
+    return rlt
+
+def create_filter_conditions(stem, dim):
+    """
+    function example: create_filter_conditions(?p, dim=["a", "b"]) ==> '&& contains(str(?p1), "a") && contains(str(?p2), "b")'
+    """
+    rlt = ""
+    for i in range(1, len(dim)+1):
+        rlt +=' && contains(str(' +stem+str(i)+ '), "'+ dim[i-1] + '")'
+    return rlt
+
+def create_str_of_vars(stem, num):
+    """
+    function example: create_str_of_vars('?d', 2) ==> '?d1 ?d2'
+    """
+    rlt = ""
+    for i in range(1, num+1):
+        rlt +=stem+str(i) + " "
+    return rlt
+        
 
 def get_all_dimensions(g):
     qstr_d = "select distinct ?s where {?s ?p ?o . filter (contains(str(?s), 'dimension'))  }"
@@ -40,6 +90,21 @@ def get_code_list_of_dim(g, dim):
     return get_query_result_column_n(g, qstr, col=0)
 
 
+def get_query_result_toPython(g, qstr):
+    rlt = []
+    qres = g.query(qstr)
+    for row in qres:
+        newRow=[]
+        for ele in row:
+            pele = ele.toPython() 
+            if isinstance(pele, str):
+                codeStr = pele.split('/')[-1]
+                newRow.append(try_to_turn_string_num(codeStr))
+            else:
+                newRow.append(float(pele))
+        rlt.append(newRow)
+    return rlt
+
 def get_query_result(g, qstr):
     rlt = []
     qres = g.query(qstr)
@@ -54,9 +119,36 @@ def get_query_result_column_n(g, qstr, col=0):
         rlt.append(row[col])
     return rlt
 
+def try_to_turn_string_num(aString):
+    if is_a_string_int(aString):
+        return int(aString)
+    elif is_a_string_float(aString):
+        return float(aString)
+    else:
+        return aString
+
+def is_a_string_int(aString):
+  try:
+    int(aString)
+    return True
+  except ValueError:
+    return False
+
+def is_a_string_float(aString):
+  try:
+    float(aString)
+    return True
+  except ValueError:
+    return False
+
 if __name__ == "__main__":
     rdffile = '../Data/aragon-2006-income.rdf'
     g = rdflib.Graph()
-    g.parse(rdffile) 
-    for row in get_all_observations(g):
+    g.parse(rdffile)  
+    dim = ["fundingClassification", "economicClassification"] 
+    qstr = qstr_for_rdf_to_one_table(g, dim=dim)
+    print(qstr)
+    rlt = get_query_result_toPython(g, qstr)
+    for row in rlt:
         print(row)
+    
---
+++ tasks/test.py
@@ -0,0 +1,20 @@
+import unittest
+import myutil as util 
+
+class QstrTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(util.create_str_of_spo_vars('?s', varStems=['?p','?d'], num=4),
+                         "?s ?p1 ?d1 . ?s ?p2 ?d2 . ?s ?p3 ?d3 . ?s ?p4 ?d4 . ")
+        self.assertEqual(util.create_filter_conditions('?p', dim=["a", "b"]),
+                        ' && contains(str(?p1), "a") && contains(str(?p2), "b")')
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
