74b00d6851ea11b479cb5fda87ad5cf3ccf8b4bf
mluk <maiklukasch@gmail.com>
2016-09-19 11:00:08 +0200

    refactored outlier detection preprocessing

--- tasks/preprocessing/send_request.py
+++ tasks/preprocessing/send_request.py
@@ -13,6 +13,7 @@ from abc import ABCMeta, abstractmethod
 
 
 class SparqlHelper(metaclass=ABCMeta):
+    # Constants:
     __URL = "http://eis-openbudgets.iais.fraunhofer.de/fuseki/sparql"
     __HEADERS = {"Accept": "text/csv"}
 
@@ -22,7 +23,7 @@ class SparqlHelper(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def _postprocess_sparql_result(self):
+    def _postprocess_sparql_result(self, sparql_result):
         """Implemented by subclasses"""
         pass
 
@@ -31,17 +32,32 @@ class SparqlHelper(metaclass=ABCMeta):
         response = requests.get(self.__URL, headers=self.__HEADERS, params=params)
         return response.text
 
+    def __write_csv_to_file(self, text, path_output_folder, file_name=None):
+        file_name = file_name if file_name else \
+            str(datetime.datetime.utcnow()) \
+                .replace(" ", "_") \
+                .replace(".", "-") \
+                .replace(":", "-")
+        file_path = os.path.join(path_output_folder, "%s.csv" % file_name)
+        with open(file_path, 'a') as file:
+            file.write(text)
+        return file_path
+
     def create_csv_as_text(self, datasets, columns, dict_cols2aggr, limit=-1):
         """
         Creates a Sparql-Query & sends it to the Sparql-Fhg-endpoint & returning the result as csv text.
         :return: The CSV Text as String for DataMining Input.
         """
         # (1) Create Sparql-Query: Needs to specified by subclasses
-        sparql_query = self._create_sparql_query(datasets, datasets, columns, dict_cols2aggr, limit=-1)
+        sparql_query = self._create_sparql_query(datasets, columns, dict_cols2aggr, limit)
+        print("Sparql-Query: %s" % sparql_query)
         # (2) Send Sparql-Query to Endpoint:
         sparql_result = self.__send_to_sparql_endpoint(sparql_query)
-        # (3) Posprocess Sparql-Query-result: Needs to specified by subclasses
+        print("Sparql-Query-Result: %s" % sparql_result)
+        # (3) Postprocess Sparql-Query-result: Needs to specified by subclasses
         csv_result = self._postprocess_sparql_result(sparql_result)
+        print("CSV-Result: %s" % csv_result)
+
         return csv_result
 
     def create_csv_as_file(self, datasets, columns, dict_cols2aggr, path_output_folder, file_name=None, limit=-1):
@@ -49,25 +65,18 @@ class SparqlHelper(metaclass=ABCMeta):
         Creates a Sparql-Query & sends it to the Sparql-Fhg-endpoint & returning the result as csv file.
         :return: The CSV file path as String for DataMining Input.
         """
-        file_name = file_name if file_name else \
-            str(datetime.datetime.utcnow()) \
-                .replace(" ", "_") \
-                .replace(".", "-") \
-                .replace(":", "-")
-        file_path = os.path.join(path_output_folder, "%s.csv" % file_name)
-        csv_result = self.create_csv_as_text()
-        with open(file_path, 'a') as file:
-            file.write(csv_result)
+        csv_result = self.create_csv_as_text(self, datasets, columns, dict_cols2aggr, limit)
+        file_path = self.__write_csv_to_file(self, csv_result, path_output_folder, file_name)
         return file_path
 
 
 class SparqlCEHelper(SparqlHelper):
-    _dict_col2types = {'ID': 'id', 'observation': 'id', 'amount': 'target', 'economicClass': 'nominal',
+    _DICT_COL_2_TYPES = {'ID': 'id', 'observation': 'id', 'amount': 'target', 'economicClass': 'nominal',
                        'adminClass': 'nominal',
                        'year': 'nominal',
                        'budgetPhase': 'nominal'}
 
-    _dict_col2model = {'amount': '?observation obeu-measure:amount ?amount2 .',
+    _DICT_COL_2_MODEL = {'amount': '?observation obeu-measure:amount ?amount2 .',
                        'year': '?observation qb:dataSet/obeu-dimension:fiscalYear ?year .',
                        'budgetPhase': '?observation gr-dimension:budgetPhase ?budgetPhase .',
                        'observation': '?slice qb:observation ?observation .',
@@ -75,11 +84,11 @@ class SparqlCEHelper(SparqlHelper):
                        'adminClass': '?slice gr-dimension:administrativeClassification ?adminClass .'
                        }
 
-    _dict_col2names = {'observation': 'ID', 'amount': 'amount', 'economicClass': 'economicClass',
+    _DICT_COL_2_NAMES = {'observation': 'ID', 'amount': 'amount', 'economicClass': 'economicClass',
                        'adminClass': 'adminClass',
                        'year': 'year', 'budgetPhase': 'budgetPhase'}
 
-    _prefixes = [
+    _PREFIXES = [
         "PREFIX obeu-measure:     <http://data.openbudgets.eu/ontology/dsd/measure/>",
         "PREFIX obeu-dimension:   <http://data.openbudgets.eu/ontology/dsd/dimension/>",
         "PREFIX qb:               <http://purl.org/linked-data/cube#>",
@@ -87,36 +96,29 @@ class SparqlCEHelper(SparqlHelper):
         "PREFIX gr-dimension: <http://data.openbudgets.eu/ontology/dsd/greek-municipalities/dimension/>"
     ]
 
-    # Helper functions:
-    def _create_select(self, datasets, columns, dict_cols2aggr, limit=-1):
-        result = "\n".join(SparqlCEHelper._prefixes)
+    def _create_sparql_query(self, datasets, columns, dict_cols2aggr={}, limit=-1):
+        """Implemented by subclasses"""
+        result = "\n".join(SparqlCEHelper._PREFIXES)
         result += "\nSELECT"
         # columns:
         for col in columns:
             aggregation = dict_cols2aggr.get(col, "")
             if aggregation:
-                result += "\n(%s(?%s) AS ?%s)" % (aggregation, col, SparqlCEHelper._dict_col2names[col])
+                result += "\n(%s(?%s) AS ?%s)" % (aggregation, col, SparqlCEHelper._DICT_COL_2_NAMES[col])
             else:
                 result += "\n?%s" % col
         # datsets:
         for dataset in datasets:
             result += "\nFROM %s" % dataset
         # where-clause:
-        result += "\nWHERE { %s }" % "\n".join([SparqlCEHelper._dict_col2model.get(col, "") for col in columns])
+        result += "\nWHERE { %s }" % "\n".join([SparqlCEHelper._DICT_COL_2_MODEL.get(col, "") for col in columns])
         # groupBy-clause:
         result += "\nGROUP BY %s" % " ".join(["?%s" % col for col in columns if col not in dict_cols2aggr.keys()])
         if limit > -1:
             result += "\nLIMIT " + str(limit)
         return result
 
-    def _create_type_mapping_line(self, csvText):
-        lines = csvText.splitlines()
-        tokens = lines[0].split(",")
-        types = [SparqlCEHelper._dict_col2types[col] for col in tokens]
-        return ",".join(types)
-
-    # Send Request:
-    def create_csv_for_outlier_text(self, datasets, columns, dict_cols2aggr, limit=-1):
+    def _postprocess_sparql_result(self, sparql_result):
         """Creates a Sparql-Query for the DM-outlier-algorithm of Christiane,
         sends it to the Sparql-endpoint at FHG-server and returns the result in csv as a String.
 
@@ -131,39 +133,16 @@ class SparqlCEHelper(SparqlHelper):
             Raises:
                 todo
         """
-        query = SparqlCEHelper._create_select(datasets, columns, dict_cols2aggr, limit=limit)
-        params = {"query": query}
-        req = requests.get(SparqlCEHelper._url, headers=SparqlCEHelper._headers, params=params)
-        csvText = req.text
-        print('csvText', csvText)
-        csvLines = csvText.splitlines()
-        csvLines.insert(1, SparqlCEHelper._create_type_mapping_line(csvText))
-        return "\n".join(csvLines)
+        csv_lines = sparql_result.splitlines()
+        csv_lines.insert(1, SparqlCEHelper._create_type_mapping_line(sparql_result))
+        return "\n".join(csv_lines)
 
-    def create_csv_for_outlier_file(self, datasets, columns, dict_cols2aggr, path_output_folder, limit=-1):
-        """Creates a Sparql-Query for the DM-outlier-algorithm of Christiane,
-        sends it to the Sparql-endpoint at FHG-server and returns the result in csv as a file named with the current timestamp in the specified folder.
-
-            :param datasets: list
-                list of the datasets which the Sparql-Query will query the results, for example ["<http://data.openbudgets.eu/resource/dataset/budget-kilkis-expenditure-2012>", "<http://data.openbudgets.eu/resource/dataset/budget-kilkis-expenditure-2014>"]
-            :param columns: list
-                list of columns OBEU-datamodel, for example ["observation", "amount", "economicClass", "adminClass", "year", "budgetPhase"]
-            :param dict_cols2aggr: dict
-                dictionary containing the mapping between columns and their aggregation for example {"observation": "MIN", "amount": "SUM"}
-            :param path_output_folder: str
-                folder-path as a String where the CSV-file is placed for example /home/mluk
-            :return: str
-                the absolute file-path as String of the CSV-file which contains the Sparql-Query-result in csv-format
-            Raises:
-                todo
-        """
-        csvText = self.create_csv_for_outlier_text(datasets, columns, dict_cols2aggr, limit=limit)
-        fileName = str(datetime.datetime.utcnow()).replace(" ", "_").replace(".", "-").replace(":", "-")
-        filePath = os.path.join(path_output_folder, "%s.csv" % fileName)
-        with open(filePath, 'a') as the_file:
-            the_file.write(csvText)
-        return filePath
-
+    @staticmethod
+    def _create_type_mapping_line(csv_text):
+        lines = csv_text.splitlines()
+        tokens = lines[0].split(",")
+        types = [SparqlCEHelper._DICT_COL_2_TYPES[col] for col in tokens]
+        return ",".join(types)
 
 # Tests:
 if __name__ == '__main__':
@@ -173,8 +152,8 @@ if __name__ == '__main__':
     input_datasets = ["<http://data.openbudgets.eu/resource/dataset/budget-kilkis-expenditure-2012>",
                       "<http://data.openbudgets.eu/resource/dataset/budget-kilkis-expenditure-2013>",
                       "<http://data.openbudgets.eu/resource/dataset/budget-kilkis-expenditure-2014>"]
-    SparqlHelperCE = SparqlCEHelper()
-    print(SparqlHelperCE._create_select(input_datasets, input_cols, input_dict_cols2aggr, limit=1))
+    sparql_helper_ce = SparqlCEHelper()
+    print(sparql_helper_ce.create_csv_as_text(input_datasets, input_cols, input_dict_cols2aggr, limit=100))
     # print(create_csv_for_outlier_text(input_datasets, input_cols, input_dict_cols2aggr))
-    print(SparqlHelperCE.create_csv_for_outlier_file(input_datasets, input_cols, input_dict_cols2aggr,
-                                                     os.path.expanduser("~"), limit=1))
+    #print(sparql_helper_ce.create_csv_as_file(input_datasets, input_cols, input_dict_cols2aggr,
+    #                                                 os.path.expanduser("~"), limit=1))
